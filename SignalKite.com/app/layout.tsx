c0ed9dfa8e470c38291af65b8352b880ea15b9f4
Sunghyun Cho <hey@cho.sh>
2024-12-07 17:47:16 +0900

    Use Shadcn ☹️

--- SignalKite.com/app/layout.tsx
+++ SignalKite.com/app/layout.tsx
@@ -3,7 +3,6 @@ import './globals.css'
 import { clsx } from 'clsx'
 import type { Metadata } from 'next'
 import { pretendard } from './fonts'
-import { Theme, ThemePanel } from '@radix-ui/themes'
 
 export const metadata: Metadata = {
   title: 'SignalKite',
@@ -18,10 +17,7 @@ export default function Root({
   return (
     <html lang='en'>
       <body className={clsx(`${pretendard.className}`, 'antialiased')}>
-        <Theme accentColor='tomato' grayColor='sand' radius='medium'>
-          {children}
-        </Theme>
-        {process.env.NODE_ENV === 'development' && <ThemePanel />}
+        {children}
       </body>
     </html>
   )
