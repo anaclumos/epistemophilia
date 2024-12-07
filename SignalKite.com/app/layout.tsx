c15e8f13698371549ac3e5db8d4c516914106316
Sunghyun Cho <hey@cho.sh>
2024-12-07 18:35:35 +0900

    style: Update import statements and fix formatting in layout file

--- SignalKite.com/app/layout.tsx
+++ SignalKite.com/app/layout.tsx
@@ -1,23 +1,74 @@
-import './globals.css'
-
-import { clsx } from 'clsx'
-import type { Metadata } from 'next'
+import { Label } from '@/components/ui/label'
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarInput,
+  SidebarInset,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarRail,
+  SidebarTrigger,
+} from '@/components/ui/sidebar'
+import { cn } from '@/lib/utils'
+import { GalleryVerticalEnd, Search } from 'lucide-react'
 import { pretendard } from './fonts'
+import './globals.css'
+import Image from 'next/image'
+import { site } from './site'
 
-export const metadata: Metadata = {
-  title: 'SignalKite',
-  description: 'SignalKite',
-}
-
-export default function Root({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+export default function Root({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <body className={clsx(`${pretendard.className}`, 'antialiased')}>
-        {children}
+      <body className={cn(`${pretendard.className}`, 'antialiased')}>
+        <SidebarProvider>
+          <Sidebar>
+            <SidebarHeader>
+              <SidebarMenu>
+                <SidebarMenuItem>
+                  <SidebarMenuButton size='lg'>
+                    <Image src={site.logo} alt='Logo' width={32} height={32} />
+                    <div className='flex flex-col gap-0.5 leading-none'>
+                      <span className='font-bold text-xl tracking-tight'>
+                        {site.title}
+                      </span>
+                    </div>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              </SidebarMenu>
+            </SidebarHeader>
+            <SidebarContent>
+              {site.nav.map((section) => (
+                <SidebarGroup key={section.title}>
+                  <SidebarGroupLabel>{section.title}</SidebarGroupLabel>
+                  <SidebarGroupContent>
+                    <SidebarMenu>
+                      {section.items.map((item) => (
+                        <SidebarMenuItem key={item.title}>
+                          <SidebarMenuButton asChild>
+                            <a href={item.url}>{item.title}</a>
+                          </SidebarMenuButton>
+                        </SidebarMenuItem>
+                      ))}
+                    </SidebarMenu>
+                  </SidebarGroupContent>
+                </SidebarGroup>
+              ))}
+            </SidebarContent>
+            <SidebarRail />
+          </Sidebar>
+          <SidebarInset>
+            <div className='w-full p-2'>
+              <SidebarTrigger className='block md:hidden' />
+            </div>
+            {children}
+          </SidebarInset>
+        </SidebarProvider>
       </body>
     </html>
   )
---
+++ SignalKite.com/app/site.ts
@@ -0,0 +1,43 @@
+export const site = {
+  title: 'SignalKite',
+  logo: '/logo.svg',
+  description: 'SignalKite',
+  nav: [
+    {
+      title: 'Explore',
+      url: '#',
+      items: [
+        {
+          title: 'Link 1',
+          url: '#',
+        },
+        {
+          title: 'Link 2',
+          url: '#',
+        },
+      ],
+    },
+    {
+      title: 'My Account',
+      url: '#',
+      items: [
+        {
+          title: 'Kites',
+          url: '#',
+        },
+        {
+          title: 'Schedules',
+          url: '#',
+        },
+        {
+          title: 'Notification Channels',
+          url: '#',
+        },
+        {
+          title: 'Postprocessing Prompts',
+          url: '#',
+        },
+      ],
+    },
+  ],
+}
